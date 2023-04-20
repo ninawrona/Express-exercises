d85099832353ecf538cc1ce3aada338737aa0fcd
ninawrona <ninawrona@gmail.com>
2023-04-20 21:17:34 +0200

    3rd exercise

---
+++ LoggingMiddleware/index.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { POKEMON } from "./pokemon.js"
+
+const app = express()
+
+app.get("/:id", (req, res) => {
+    const reqId = parseInt(req.params.id)
+    const pokemon = POKEMON.find(poke => poke.id === reqId)
+    if (pokemon) res.send(pokemon)
+    else res.status(404).send({ msg: "No users with the id of " + reqId })
+})
+
+app.listen(3000)
