cdb6003f4b0eaa17f4b72c7d4459475f4354cd29
ninawrona <ninawrona@gmail.com>
2023-04-24 10:25:26 +0200

    full login web application
    works

---
+++ ConsumingAPIFromFrontend/Users.js
@@ -0,0 +1,8 @@
+const users = [
+    {
+        username: "Nina",
+        password: "$2a$10$vXduDKE6mvTK.gPWamMOtO9ej76mTdaOicqYyDUie/7LMkv/l8Vwe" //pasword123 hashed 10 rounds
+    }
+]
+
+export {users as default}
